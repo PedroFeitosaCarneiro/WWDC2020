0357bf2c2400ffe686bdf41525e38961ed6af3d3
Pedro Paulo <Pedro.feitosa.carneiro@gmail.com>
2020-03-27 14:45:18 -0300

    template inicial criado

--- WWDC2020.playground/Contents.swift
+++ WWDC2020.playground/Contents.swift
@@ -1,10 +1,12 @@
+//: A SpriteKit based Playground
+
 import PlaygroundSupport
 import SpriteKit
-import Cocoa
 
 let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 800, height: 800))
+if let scene = GameScene(fileNamed: "GameScene") {
+    scene.scaleMode = .aspectFill
+    sceneView.presentScene(scene)
+}
 
-let scene = SKScene(size: CGSize(width: 800, height: 800))
-sceneView.showsFPS = true
-sceneView.presentScene(scene)
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
